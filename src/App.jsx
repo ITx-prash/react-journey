d48a052cbe620d76bbcec409560fdadb7625a790
Prashant Adhikari <prashantadhikari440@gmail.com>
2025-12-26 23:38:22 +0545

    chore: format code for consistency in App and main files

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ const App = () => {
           element={
             <>
               <NavHeader />
-              <Weather/>
+              <Weather />
             </>
           }
         />
